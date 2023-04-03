e8dfa0e21f66f3ba79088aaa6ce395d016559673
bashar <waez@compra.de>
2023-04-03 15:56:05 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      Hallo World!
+    </div>
+  )
+}
+
+export default App
+
+
+// ## Installed ## //
+
+// VITE
+// REACT-ROUTER-DOM
+// React-Icons
+// SASS
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import ErrorPage from "./ErrorPage";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
